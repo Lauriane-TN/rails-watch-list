b29871cc713fdd07ccb1ad32417e375a6bee1bfe
Lauriane NATHOU <laurianenathou@gmail.com>
2021-11-12 12:05:26 +0100

    Application fonctionnelle with front

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+object_serialized = URI.open(url).read
+object = JSON.parse(object_serialized)
+movies = object['results']
+
+movies.each do |program|
+  title = program['title']
+  overview = program['overview']
+  rating = program['vote_average']
+  poster_url = "https://image.tmdb.org/t/p/w500#{program['poster_path']}"
+  Movie.create(title: title, overview: overview, rating: rating, poster_url: poster_url)
+end
